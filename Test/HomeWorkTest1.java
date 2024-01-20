181fc4f385ba2d2b4fa233f01749b9bb20b40825
Temirlan-artman-888 <temiramanas@gmail.com>
2024-01-20 22:47:38 +0200

    HomeWork was made using the Exception methods update

--- Test/HomeWorkTest1.java
+++ Test/HomeWorkTest1.java
@@ -2,8 +2,6 @@ import lesson_25.IncorrectFigureSizeException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class HomeWorkTest1 {
 
     @Test
@@ -15,17 +13,17 @@ class HomeWorkTest1 {
     }
 
     @Test
-    void calculateRectangleArea() throws IncorrectFigureSizeException{
+    void calculateRectangleArea() throws IncorrectFigureSizeException {
         double expected = 25;
 
-        double result = HomeWork.calculateRectangleArea(new double[]{5,5});
+        double result = HomeWork.calculateRectangleArea(new double[]{5, 5});
         Assertions.assertEquals(expected, result);
     }
 
     @Test
-    void calculateTriangleArea() throws IncorrectFigureSizeException{
+    void calculateTriangleArea() throws IncorrectFigureSizeException {
         double expected = 1.7320508075688772;
-        double result = HomeWork.calculateTriangleArea(new double[]{2,2,2});
+        double result = HomeWork.calculateTriangleArea(new double[]{2, 2, 2});
         Assertions.assertEquals(expected, result);
     }
 }
